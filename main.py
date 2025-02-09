2abc66f4fc8cd24f29629a111f5a2a9bc3c26a9c
abarajas11 <abarajas@yofio.co>
2025-02-09 10:58:53 -0600

    new bool field added for each model

--- main.py
+++ main.py
@@ -3,10 +3,8 @@ from pydantic import BaseModel
 import pickle
 import pandas as pd
 
-app = FastAPI(
-    title="API de Predicción de Riesgo de Comorbilidades",
-    description="API para predecir el riesgo de obesidad, diabetes e hipertensión."
-)
+app = FastAPI(title="API de Predicción de Riesgo de Comorbilidades")
+
 
 # --- Cargar modelos y scalers específicos ---
 try:
@@ -54,9 +52,23 @@ class PatientHypertensionData(BaseModel):
     edad: int
 
 
-class RiskPrediction(BaseModel):
+class ObesityRiskPrediction(BaseModel):
     risk_category: str
     probability: float
+    is_obese: bool
+
+
+class DiabetesRiskPrediction(BaseModel):
+    risk_category: str
+    probability: float
+    is_diabetic: bool
+
+
+class HypertensionRiskPrediction(BaseModel):
+    risk_category: str
+    probability: float
+    is_hypertensive: bool
+
 
 # --- Función de utilidad para categorizar riesgo ---
 
@@ -72,26 +84,42 @@ def categorize_risk(probability: float, low_medium_threshold: float = 0.5, mediu
 # --- Endpoints de la API ---
 
 
-@app.post("/predict/obesity", response_model=RiskPrediction, tags=["Obesidad"])
+@app.post("/predict/obesity", response_model=ObesityRiskPrediction, tags=["Obesidad"])
 async def predict_obesity(patient_data: PatientObesityData):
+    """
+    Endpoint para la predicción de riesgo de obesidad.
+
+    Este endpoint recibe datos de un paciente y predice su riesgo de obesidad.
+    Utiliza un modelo de machine learning entrenado para realizar la predicción.
+    """
     try:
         df_patient = pd.DataFrame([patient_data.dict()])
         features = ['imc', 'ldl', 'trigliceridos',
                     'glucosa_ayunas', 'insulina', 'condicion_genetica']
         # Aplicar el scaler específico para obesidad
         df_patient[features] = scaler_obesity.transform(df_patient[features])
-        print("DataFrame escalado para obesidad:", df_patient[features])
         X_patient = df_patient[features]
-        probability = obesity_model.predict_proba(X_patient)[:, 1][0]
-        risk_category = categorize_risk(probability)
-        return {"risk_category": risk_category, "probability": float(probability)}
+        prediction = obesity_model.predict(X_patient)[0]
+        obesity_probability = obesity_model.predict_proba(X_patient)[0][1]
+        is_obese = bool(prediction)
+        risk_category = categorize_risk(obesity_probability)
+        return {"risk_category": risk_category,
+                "probability": float(obesity_probability),
+                "is_obese": is_obese
+                }
     except Exception as e:
         raise HTTPException(
             status_code=500, detail=f"Error al procesar la solicitud de obesidad: {e}")
 
 
-@app.post("/predict/diabetes", response_model=RiskPrediction, tags=["Diabetes"])
+@app.post("/predict/diabetes", response_model=DiabetesRiskPrediction, tags=["Diabetes"])
 async def predict_diabetes(patient_data: PatientDiabetesData):
+    """
+    Endpoint para la predicción de riesgo de diabetes.
+
+    Este endpoint recibe datos de un paciente y predice su riesgo de diabetes.
+    Utiliza un modelo de machine learning entrenado para realizar la predicción.
+    """
     try:
         df_patient = pd.DataFrame([patient_data.dict()])
         features = ['glucosa_ayunas', 'insulina',
@@ -99,27 +127,44 @@ async def predict_diabetes(patient_data: PatientDiabetesData):
         # Aplicar el scaler específico para diabetes
         df_patient[features] = scaler_diabetes.transform(df_patient[features])
         X_patient = df_patient[features]
-        probability = diabetes_model.predict_proba(X_patient)[:, 1][0]
-        risk_category = categorize_risk(probability)
-        return {"risk_category": risk_category, "probability": float(probability)}
+        prediction = diabetes_model.predict(X_patient)[0]
+        diabetes_probability = diabetes_model.predict_proba(X_patient)[0][1]
+        is_diabetic = bool(prediction)
+        risk_category = categorize_risk(diabetes_probability)
+        return {"risk_category": risk_category,
+                "probability": float(diabetes_probability),
+                "is_diabetic": is_diabetic
+                }
     except Exception as e:
         raise HTTPException(
             status_code=500, detail=f"Error al procesar la solicitud de diabetes: {e}")
 
 
-@app.post("/predict/hipertension", response_model=RiskPrediction, tags=["Hipertension"])
+@app.post("/predict/hipertension", response_model=HypertensionRiskPrediction, tags=["Hipertension"])
 async def predict_hipertension(patient_data: PatientHypertensionData):
+    """
+    Endpoint para la predicción de riesgo de hipertensión.
+
+    Este endpoint recibe datos de un paciente y predice su riesgo de hipertensión.
+    Utiliza un modelo de machine learning entrenado para realizar la predicción.
+    """
     try:
         df_patient = pd.DataFrame([patient_data.dict()])
         features = ['presion_arterial_sistolica', 'presion_arterial_diastolica',
                     'creatinina', 'ldl', 'condicion_genetica', 'edad']
-        # Aplicar el scaler específico para hipertensión
+        # Aplicar el scaler específico para hipertension
         df_patient[features] = scaler_hipertension.transform(
             df_patient[features])
         X_patient = df_patient[features]
-        probability = hipertension_model.predict_proba(X_patient)[:, 1][0]
-        risk_category = categorize_risk(probability)
-        return {"risk_category": risk_category, "probability": float(probability)}
+        prediction = hipertension_model.predict(X_patient)[0]
+        hipertension_probability = hipertension_model.predict_proba(X_patient)[
+            0][1]
+        is_hypertensive = bool(prediction)
+        risk_category = categorize_risk(hipertension_probability)
+        return {"risk_category": risk_category,
+                "probability": float(hipertension_probability),
+                "is_hypertensive": is_hypertensive
+                }
     except Exception as e:
         raise HTTPException(
             status_code=500, detail=f"Error al procesar la solicitud de hipertensión: {e}")
